55300039a4bd7dff1219fcf6f0b165a74409cf2f
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2026-02-23 13:37:44 +0330

    Deploying on Vercel cloud

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import {createServer} from 'node:http'
-import {AddressInfo} from "node:net"
+import type {VercelRequest, VercelResponse} from '@vercel/node'
 
-const server = createServer((req, res) => {
-    console.info(req.method, req.url)
-    if (req.method === 'GET' && req.url === '/') return res.end('Hello World!')
-    res.statusCode = 404
-    res.end()
-})
-
-server.listen(7799)
-
-console.info('Server started on', (server.address() as AddressInfo).port)
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  res.status(200).send('hello world')
+}
